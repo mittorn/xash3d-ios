b419e7cbb3b90311521ed2727da253073b851411
mittorn <mittorn@sibmail.com>
2018-04-08 09:35:49 +0600

    Add prepare and build script

---
+++ scripts/prepare-and-build-short.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sh scripts/fetch-projects.sh
+sh scripts/fill-project.sh
+sh scripts/build-mainui.sh iphoneos armv7
+cp mods-build/output/armv7/* prebuilt/armv7/*
+sh scripts/run-xcodebuild.sh iphoneos armv7 output-armv7.ipa
+sh scripts/build-mainui.sh iphoneos arm64
+cp mods-build/output/arm64/* prebuilt/arm64/*
+sh scripts/run-xcodebuild.sh iphoneos armv7 output-arm64.ipa
