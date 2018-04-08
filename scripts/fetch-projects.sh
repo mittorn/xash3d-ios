6f6b50fb58ccb7d39ac7800c238847c00cfed68e
mittorn <mittorn@sibmail.com>
2018-04-08 09:22:50 +0600

    Fix project fetch

--- scripts/fetch-projects.sh
+++ scripts/fetch-projects.sh
@@ -4,4 +4,4 @@ git clone https://github.com/FWGS/nanogl --depth 1
 git clone https://github.com/FWGS/microndk --depth 1
 wget http://libsdl.org/release/SDL2-2.0.5.tar.gz
 tar xzf SDL2-2.0.5.tar.gz
-mv SDL2-2.0.5.tar.gz SDL
+mv SDL2-2.0.5 SDL
