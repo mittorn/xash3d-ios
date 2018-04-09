19fda259c0c087f4b5858eba2f6ada1048792dfa
mittorn <mittorn@sibmail.com>
2018-04-09 23:51:29 +0600

    Build mods, add deploy script

---
+++ scripts/build-all-mods.sh
@@ -0,0 +1,44 @@
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d bshift _bshift
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d CAd _CAd
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d aomdc _AoMDC
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d theyhunger _Hunger
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d sohl1.2 _Spirit
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d tot _tot
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d bubblemod _bubblemod
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d eftd _eftd
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d visitors _visitors
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d residual_point _rp
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d rebellion _rebellion
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d blackops _blackops
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d bot10 _bot10
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d thegate _TheGate
+sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/nekonomicon/hlsdk-xash3d poke646 _poke646
+#broken
+#sh scripts/build-hlsdk.sh iphoneos arm64 https://github.com/H-I-N-G/hlsdk-xash3d opfor _gearbox
+
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d bshift _bshift
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d dmc _dmc
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d CAd _CAd
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d aomdc _AoMDC
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d theyhunger _Hunger
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d sohl1.2 _Spirit
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d tot _tot
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d bubblemod _bubblemod
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d eftd _eftd
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d visitors _visitors
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d residual_point _rp
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d rebellion _rebellion
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d blackops _blackops
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d bot10 _bot10
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d noffice _noffice
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d poke646 _poke646
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d poke646_vendetta _vendetta
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d thegate _TheGate
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d half-screwed _half-screwed
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/hlsdk-xash3d aghl _ag
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/SoHL-1.2 halloween _shall
+sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/nekonomicon/SoHL-1.2 zombie-x _Zombie-X-DLE
+# not hlsdk
+#sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/FWGS/QuakeRemakeDevkit master _ID1
+# broken
+#sh scripts/build-hlsdk.sh iphoneos armv7 https://github.com/H-I-N-G/hlsdk-xash3d opfor _gearbox
