5959f15178741794c6c1312d762c7ae51a79eaba
mittorn <mittorn@sibmail.com>
2018-04-10 03:06:48 +0700

    Fix script

--- scripts/prepare-and-build-fast.sh
+++ scripts/prepare-and-build-fast.sh
@@ -6,4 +6,4 @@ cp mods-build/output/armv7/* prebuilt/armv7/*
 sh scripts/run-xcodebuild.sh iphoneos armv7 output-armv7.ipa
 sh scripts/build-mainui.sh iphoneos arm64
 cp mods-build/output/arm64/* prebuilt/arm64/*
-sh scripts/run-xcodebuild.sh iphoneos armv7 output-arm64.ipa
+sh scripts/run-xcodebuild.sh iphoneos arm64 output-arm64.ipa
